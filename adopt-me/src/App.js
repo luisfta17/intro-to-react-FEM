90b21b098f59dfc0c332de5a0f3406087e4f8fed
Luis Tejero <luis@Luiss-MacBook-Pro.local>
2019-07-24 11:43:27 +0100

    first commit

---
+++ adopt-me/src/App.js
@@ -0,0 +1,39 @@
+const Pet = ({name, animal, breed}) => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, name),
+      React.createElement("h2", {}, animal),
+      React.createElement("h2", {}, breed)
+    ]);
+  };
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Ado pt Me!"),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "Dog",
+                breed: "Havanese"
+                }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "Bird",
+                breed: "Cocktatiel"
+                }),
+            React.createElement(Pet, {
+                name: "Doink",
+                animal: "Cat",
+                breed: "Mix"
+                })
+        ]
+
+        );
+    };
+
+ReactDOM.render(
+        React.createElement(App),
+        document.getElementById("root")
+        );
+            
